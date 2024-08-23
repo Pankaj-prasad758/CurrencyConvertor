51570a766dde971ff08c8aaac6652c80014afc77
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-08-23 17:45:41 +0530

    done

---
+++ CurrencyConvertor/src/App.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { InputBox } from "./component";
+import useCurrencyInfo from "./hooks/useCurrencyinfo";
+
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertAmount, setConvertAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertAmount(amount);
+    setAmount(convertAmount);
+  };
+
+  const convert = () => {
+    setConvertAmount(amount * currencyInfo[to]);
+  };
+
+  return (
+    <>
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage:`url('https://img.freepik.com/free-vector/stock-graph-market-finance-chart-growth-business-blue-bond-data-online-bank_333792-77.jpg?w=1060&t=st=1724393267~exp=1724393867~hmac=f1dbfcceecd15a1e4f00045d791dec9c24f4167a250375f38b882c36bb874fa0')`
+        }}
+      >
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+             onClick={(e) => {
+              e.preventDefault();
+              convert()
+             }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) =>
+                     setAmount(amount)}
+                  selectCurrency={from} 
+                  onAmountChange={(amount)=> setAmount(amount)}                 
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                 label="to"
+                 amount={convertAmount}
+                 currencyOptions={options}
+                 onCurrencyChange={(currency) => setTo(currency)}
+                 selectCurrency={from}  
+                 amountDisable
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
